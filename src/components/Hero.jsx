8550e9c3d9d7ce09e8780d93c9900cfb4c795865
david <david.gonzalez@gradiweb.com>
2023-02-23 16:09:11 -0500

    fix: ajustar docs deploy v12

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,33 @@
 export const Hero = ({ data }) => {
-	console.log(data)
+	console.log(data);
 	return (
 		<section aria-label="main" className="hero" id="main">
-			<div className="hero-image">
-
-				{/*        
-                    <a target="_blank" className="button-icon-foto" href="https://platzi.com/p/bdavidegonzalez/">
-                        <h2 className="title-white">Mis certificaciones de platzi. </h2> 
-                    </a>
-                */}
-
-				<img src={ data[0].img_profile } width="338" height="330" alt="" />
-			</div>
-
+			
+				<div className="hero-image">
+				<a target="_blank" className="button-icon-foto" href="https://platzi.com/p/bdavidegonzalez/">
+					<img src={data[0].img_profile} width="338" height="330" alt="" />
+					</a>
+				</div>
+			
 			<div className="hero-description">
 				<h1>
-					<span>{ data[0].title }</span>
-					<span>{ data[0].name }</span>
+					<span>{data[0].title}</span>
+					<span>{data[0].name}</span>
 				</h1>
-				<p>{ data[0].description }</p>
+				<p>{data[0].description}</p>
 				<div className="social">
 					<a target="_blank" className="button-icon" href="https://github.com/bdavidegonzalez">
-						<img src={ data[0].github } />
+						<img src={data[0].github} />
 					</a>
 					<a
 						target="_blank"
 						className="button-icon"
 						href="https://www.linkedin.com/in/david-gonzalez-69097016a/"
 					>
-						<img src={ data[0].linkedin } />
+						<img src={data[0].linkedin} />
 					</a>
 				</div>
-                {/*				
+				{/*				
                     <a href="#" className="button">
                             Descargar Hoja de vida.
                     </a> 
